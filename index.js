31007bf891839b9ff14dc26c9e7af3605fa7d2c0
jkarnowski <jaclyn.karnowski@gmail.com>
2016-06-15 20:08:47 -0700

    adds lodash

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express()
 
+var fs = require('fs')
+var _ = require('lodash')
+var users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function(error, data){
+	if (error) throw error
+
+		JSON.parse(data).forEach(function(user){
+			user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+			users.push(user)
+		})
+})
+
 // when you get an HTTP GET, call this function
 app.get('/', function(req, res){
-	res.send('ola world!')
+	// res.send('ola world!')
+	// what are these arguments?
+	res.send(JSON.stringify(users, null, 2))
 })
 
 app.get('/yo', function(req, res){
@@ -11,5 +26,5 @@ app.get('/yo', function(req, res){
 })
 
 var server = app.listen(3000, function(){
-	console.log('Server running at http://localhost:' + server.address().port)
+	console.log('Server is running at http://localhost:' + server.address().port)
 })
