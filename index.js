feb64e875d9dcd2358a596ed040f17d9a84c98e7
jkarnowski <jaclyn.karnowski@gmail.com>
2016-06-15 20:01:58 -0700

    changes route to test nodemon

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var app = express()
 
 // when you get an HTTP GET, call this function
 app.get('/', function(req, res){
-	res.send('hello world!')
+	res.send('ola world!')
+})
+
+app.get('/yo', function(req, res){
+	res.send('going to make a change')
 })
 
 var server = app.listen(3000, function(){
